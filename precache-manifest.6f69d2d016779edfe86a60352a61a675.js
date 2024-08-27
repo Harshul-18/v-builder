e8b9d7620e31a16a66aab52d7fb351889e6fcb37
Harshul-18 <harshulnanda0@gmail.com>
2024-08-27 12:32:31 +0530

    Updates

---
+++ precache-manifest.6f69d2d016779edfe86a60352a61a675.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12fe5f180de98e090770636e86fb33a",
+    "url": "/v-builder/index.html"
+  },
+  {
+    "revision": "f5e172d9d23b3c40a941",
+    "url": "/v-builder/static/css/main.eff2e01c.chunk.css"
+  },
+  {
+    "revision": "d4b67149a17c33de1cda",
+    "url": "/v-builder/static/js/2.806020d9.chunk.js"
+  },
+  {
+    "revision": "f5e172d9d23b3c40a941",
+    "url": "/v-builder/static/js/main.6817ac7b.chunk.js"
+  },
+  {
+    "revision": "3d972b519253460c5d9b",
+    "url": "/v-builder/static/js/runtime~main.7c29018b.js"
+  }
+]);
